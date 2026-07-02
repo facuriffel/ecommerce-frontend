39d5bfd3f1232fc51ccff58fc27f047b4c4a58ea
facuriffel <facundoriffel04@gmail.com>
2026-07-02 14:42:26 -0300

    Correcciones

--- src/environments/environment.cloud.ts
+++ src/environments/environment.cloud.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://ecommerce-back-monolito-bg69.onrender.com'
+    apiUrl: 'https://ecommerce-back-monolito-bg69.onrender.com/api'
 };
